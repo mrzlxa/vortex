b869ebd1998735f7bf30e344fd9ee2900cb165c9
MrFoil <demoninio@gmail.com>
2015-01-03 22:48:55 +0200

    Newfags can't in triforce

--- GL_TEST4/GL_TEST4.cpp
+++ GL_TEST4/GL_TEST4.cpp
@@ -1,48 +1,59 @@
 #include "stdafx.h"
 #include <gl/glut.h>
  
-// One-time initializtion logic
-void init( void )
-{
-    printf( "OpenGL version: %s\n", (char*)glGetString(GL_VERSION));
-    printf( "OpenGL renderer: %s\n", (char*)glGetString(GL_RENDERER));
+
  
-    //Configure basic OpenGL settings
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_BLEND);
-    glEnable(GL_TEXTURE_2D);
+
+void display()
+{
+glClear(GL_COLOR_BUFFER_BIT); /*Функция очищения экрана*/ 
+glBegin(GL_LINES);//начало рисования линий 
+  glColor3f(0.0, 1.0, 0.0); //цвет красный 
+  //первый
+  glVertex2f (0.1, 0.1);
+  glVertex2f (-0.08, -0.1);
+
+  glVertex2f (-0.08, -0.1);
+  glVertex2f (0.26, -0.1);
+
+  glVertex2f (0.26, -0.1);
+  glVertex2f (0.1, 0.1);
+  //второй
+  glVertex2f (-0.08, -0.1);
+  glVertex2f (-0.26, -0.3);
+  
+  glVertex2f (-0.26, -0.3);
+  glVertex2f (0.1, -0.3);
+  
+  glVertex2f (0.1, -0.3);
+  glVertex2f (-0.08, -0.1);
+  //а это третий
+  glVertex2f (0.26, -0.1);
+  glVertex2f (0.42, -0.3);
+  
+ glVertex2f (0.42, -0.3);
+  glVertex2f (0.1, -0.3);
+  
+  glVertex2f (0.1, -0.3);
+  glVertex2f (0.26, -0.1);
+
+  glEnd(); 
+glFlush(); /*Отрисовывает всё, что мы описали*/ 
 }
  
-// Main drawing routine. Called repeatedly by GLUT's main loop
-void display( void )
-{
-    //Clear the screen and set our initial view matrix
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
- 
-    //TODO: Perform drawing here
- 
-    //We just drew to the back buffer. Now we need to swap that with the
-    //front buffer to show it on screen.
-    glutSwapBuffers();
-}
- 
-// Entry point - GLUT setup and initialization
+
 int main( int argc, char** argv )
 {
- 
-   glutInit( &argc, argv );
-   glutInitDisplayMode (GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (800, 600);
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow( "MyWIndow" );
-   glutDisplayFunc( display );
- 
-   init();
- 
-   glutMainLoop();
- 
-   return 0;
+	glutInit(&argc, argv); 
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
+  glutInitWindowSize(600, 600); 
+  glutInitWindowPosition(100, 10); 
+  glutCreateWindow("First window!"); 
+  glClearColor(0.0, 0.0, 0.0, 0.0); 
+  glMatrixMode(GL_PROJECTION); 
+  glLoadIdentity(); 
+  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); 
+  glutDisplayFunc(display); 
+  glutMainLoop(); 
+  return 0;
 }
